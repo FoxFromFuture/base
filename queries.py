beaae16e047d0a42654c95e75ae72ff6c70fda24
Egor <egor.com91@mail.ru>
2021-12-10 19:35:17 +0300

    minor changes

--- queries.py
+++ queries.py
@@ -130,26 +130,48 @@ EXECUTE PROCEDURE tracks_count_trigger_minus_func();
 
 INSERT INTO author (name, years, country)
 VALUES
-('The Weeknd', '2012 - today', 'USA');
+('twenty one pilots', '2009 - today', 'USA');
 
 INSERT INTO album (album_title, release_year, genre, tracks_count)
 VALUES
-('After Hours', '2020-03-20', 'R&B/Soul', 0);
+('Trench', '2018-10-05', 'Alternative', 0);
 
 INSERT INTO track_list (track_title, album_id, duration, author_id)
 VALUES
-('Alone Again', 1, '00:04:10', 1),
-('Too Late', 1, '00:03:59', 1),
-('Hardest To Love', 1, '00:03:31', 1),
-('Scared To Live', 1, '00:03:11', 1),
-('Snowchild', 1, '00:04:07', 1),
-('Escape From LA', 1, '00:05:55', 1),
-('Heartless', 1, '00:03:18', 1),
-('Faith', 1, '00:04:43', 1),
-('Blinding Lights', 1, '00:03:20', 1),
-('In Your Eyes', 1, '00:03:57', 1),
-('Save Your Tears', 1, '00:03:35', 1),
-('Repeat After Me', 1, '00:03:15', 1),
-('After Hours', 1, '00:06:01', 1),
-('Until I Bleed Out', 1, '00:03:12', 1);
+('Jumpsuit', 1, '00:03:58', 1),
+('Levitate', 1, '00:02:25', 1),
+('Morph', 1, '00:04:18', 1),
+('My Blood', 1, '00:03:49', 1),
+('Chlorine', 1, '00:05:24', 1),
+('Smithereens', 1, '00:02:57', 1),
+('Neon Gravestones', 1, '00:04:00', 1),
+('The Hype', 1, '00:04:25', 1),
+('Nico and the Niners', 1, '00:03:45', 1),
+('Cut My Lip', 1, '00:04:42', 1),
+('Bandito', 1, '00:05:30', 1),
+('Pet Cheetah', 1, '00:03:18', 1),
+('Legend', 1, '00:02:52', 1),
+('Leave the City', 1, '00:04:40', 1);
+
+INSERT INTO author (name, years, country)
+VALUES
+('The xx', '2005 - today', 'Great Britain');
+
+INSERT INTO album (album_title, release_year, genre, tracks_count)
+VALUES
+('xx', '2009-08-17', 'Alternative', 0);
+
+INSERT INTO track_list (track_title, album_id, duration, author_id)
+VALUES
+('Intro', 2, '00:02:07', 2),
+('VCR', 2, '00:02:57', 2),
+('Crystalised', 2, '00:03:21', 2),
+('Islands', 2, '00:02:40', 2),
+('Heart Skipped A Beat', 2, '00:04:02', 2),
+('Fantasy', 2, '00:02:38', 2),
+('Shelter', 2, '00:04:30', 2),
+('Basic Space', 2, '00:03:08', 2),
+('Infinity', 2, '00:05:13', 2),
+('Night Time', 2, '00:03:36', 2),
+('Stars', 2, '00:04:22', 2);
 """
